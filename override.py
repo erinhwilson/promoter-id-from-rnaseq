da706115a5d87d8f7e557f5b52c677267e478bb6
Erin Wilson <ewilson6@cs.washington.edu>
2020-08-12 12:38:11 -0700

    add override curated dictionary

---
+++ override.py
@@ -0,0 +1,46 @@
+# python dict of naming overrides
+
+GENE_NAME_OVERRIDE = {
+	# pmmo Genes
+	'EQU24_RS19315':'pmoC',
+	'EQU24_RS19310':'pmoA',
+	'EQU24_RS19305':'pmoB',
+	
+	# smmo Genes
+	'EQU24_RS05930':'mmoR',
+	'EQU24_RS05925':'mmoG',
+	'EQU24_RS05910':'mmoC',
+	'EQU24_RS05900':'mmoZ',
+	'EQU24_RS05895':'mmoB',
+	'EQU24_RS05890':'mmoY',
+	'EQU24_RS05885':'mmoX',
+	
+	# groEL/groES genes
+	'EQU24_RS17825':'groS',
+	'EQU24_RS08650':'groS',
+
+	# mxaF genes (mxa and mox)
+	'EQU24_RS18155':'mxaD',
+	'EQU24_RS18140':'moxF',
+	'EQU24_RS18125':'moxI',
+	'EQU24_RS18120':'moxR',
+	'EQU24_RS18115':'mxaP', # via blast
+	'EQU24_RS18110':'mxaS',
+	'EQU24_RS18105':'mxaA',
+	'EQU24_RS18100':'mxaC',
+	'EQU24_RS18095':'mxaK',
+	'EQU24_RS18090':'mxaL',
+	'EQU24_RS18145':'mxaB',
+
+	# Xox
+	'EQU24_RS18610':'xoxJ',
+	'EQU24_RS18605':'xoxF'
+
+}
+
+
+GENE_PRODUCT_OVERRIDE = {
+	'EQU24_RS05925':'likely chaperone for smmo',	
+}
+
+
